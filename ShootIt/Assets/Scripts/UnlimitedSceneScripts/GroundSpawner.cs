93d592144ab84d4afdec3421061242650ee6f865
SzeFungTing <101374235+SzeFungTing@users.noreply.github.com>
2022-11-23 12:32:58 +0800

    up

--- ShootIt/Assets/Scripts/UnlimitedSceneScripts/GroundSpawner.cs
+++ ShootIt/Assets/Scripts/UnlimitedSceneScripts/GroundSpawner.cs
@@ -4,76 +4,86 @@ using UnityEngine;
 
 public class GroundSpawner : MonoBehaviour
 {
-    public GameObject building1;
-    public GameObject building2;
-    public GameObject building3;
+    //public GameObject building1;
+    //public GameObject building2;
+    //public GameObject building3;
+    public int buildingNumber;
+    public List<GameObject> building;
 
     public GameObject groundTile;
 
     GameObject temp;
     Vector3 nextSpawnPoint;
 
+    Quaternion buildingFacing;
+    Quaternion leftQuaternion = new Quaternion(-0.5f, 0.5f, 0.5f, 0.5f);
+    Quaternion RightQuaternion = new Quaternion(-0.5f, -0.5f, -0.5f, 0.5f);
+
     List<Vector3> points = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)
         {
             SpawnTile();
         }
-       
+
+
+        //building = new List<GameObject>(buildingNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SpawnTile()
     {
-        
 
-        /*GameObject*/ temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
-        
 
-        for(int i =0; i < 2; i++)
+        /*GameObject*/
+        temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
+
+
+        for (int i = 0; i < 2; i++)
         {
-            GameObject tempBuilding = Instantiate(RandomObject(), RandomPosition(), Quaternion.identity, temp.transform.GetChild(1));
+            GameObject tempBuilding = Instantiate(RandomObject(), RandomPosition(), buildingFacing, temp.transform.GetChild(1));
+            buildingFacing = RightQuaternion;
         }
-
         nextSpawnPoint = temp.transform.GetChild(0).transform.position;
     }
 
     GameObject RandomObject()
     {
-        int randomNum = Random.Range(0, 3);
+        int randomNum = Random.Range(0, building.Count);
 
-        switch (randomNum)
-        {
-            default:
-            case 0:
-                return building1;
-            case 1:
-                return building2;
-            case 2:
-                return building3;
-        }
+        return building[randomNum];
     }
 
     Vector3 RandomPosition()
     {
-        float randomX = Random.Range(1.5f, 6.0f);
-        if(Random.Range(1, 51)%2 == 1)                      //50% randomX -> -randomX
+        buildingFacing = RightQuaternion;
+        float randomX = Random.Range(6.0f, 6.9f);
+        if (Random.Range(1, 51) % 2 == 1)                      //50% randomX -> -randomX
         {
             randomX = -randomX;
+            BuildingFacing();
         }
 
-        float randomZ = Random.Range(5.0f, 6.0f);        //-5.0 - 6
+        float randomZ = Random.Range(2.9f, 12.9f);
         //Debug.Log("position: " + new Vector3(randomX, 1f, randomZ));
 
-        return new Vector3(randomX + temp.transform.position.x, 1f + temp.transform.position.y, randomZ + temp.transform.position.z);
+        
+
+        return new Vector3(randomX + temp.transform.position.x, temp.transform.position.y, randomZ + temp.transform.position.z);
         //Physics.CheckSphere(transform.position, sphereRadius)
+    }
+
+    Quaternion BuildingFacing()
+    {
+        Debug.Log("BuildingFacing");
+        return buildingFacing = leftQuaternion;
     }
 }

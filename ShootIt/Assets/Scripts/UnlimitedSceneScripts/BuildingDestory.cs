f271cd56491d69389f5b9e8a033df5318b321ba3
SzeFungTing <101374235+SzeFungTing@users.noreply.github.com>
2022-11-28 16:21:01 +0800

    CaveScene + little container Scene

--- ShootIt/Assets/Scripts/UnlimitedSceneScripts/BuildingDestory.cs
+++ ShootIt/Assets/Scripts/UnlimitedSceneScripts/BuildingDestory.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class BuildingDestory : MonoBehaviour
 {
     float destroyTime = 3.5f;
+    float randomY;
+    [SerializeField] private float speed = 20f;
 
     // Start is called before the first frame update
     void Start()
     {
+        randomY = Random.Range(-2f, 15f);
         Destroy(this.gameObject, destroyTime);
     }
 
+    private void Update()
+    {
+        
+        if(transform.position.y <= randomY)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+    }
+
     // Update is called once per frame
-  
+
 }
--- ShootIt/Assets/Scripts/UnlimitedSceneScripts/GroundTile.cs
+++ ShootIt/Assets/Scripts/UnlimitedSceneScripts/GroundTile.cs
@@ -7,6 +7,8 @@ public class GroundTile : MonoBehaviour
     GroundSpawner groundSpawner;
     GroundSpawner1 groundSpawner1;
 
+    [SerializeField] private float speed = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +16,17 @@ public class GroundTile : MonoBehaviour
             groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
         else
             groundSpawner1 = GameObject.FindObjectOfType<GroundSpawner1>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.y < 0)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
     }
 
     private void OnTriggerExit(Collider other)
